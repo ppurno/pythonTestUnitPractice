0e100b89588c5b726ee13b9886771f787c6db06a
ProttushPurno <p.purno@uea.ac.uk>
2022-07-26 18:11:34 +0100

    reformatted

--- creditaccount.py
+++ creditaccount.py
@@ -7,17 +7,17 @@ class CreditAccount:
 
     def get_balance(self):
         return self.itsParent.get_balance()
- 
+
     def debit(self, amt):
         bal = self.itsParent.get_balance()
         if bal - amt < 0:
             raise InsufficientFunds(str(bal) + " - " + str(amt) + " cannot be completed...")
 
         self.itsParent.debit(amt)
-        
+
         return self.itsParent.get_balance()
-    
+
     def credit(self, amt):
         self.itsParent.credit(amt)
-        
+
         return self.itsParent.get_balance()
