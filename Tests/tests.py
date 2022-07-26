569dc92691fc527b62e082d39c43b74c7e42a9ca
ProttushPurno <p.purno@uea.ac.uk>
2022-07-26 16:23:45 +0100

    Third Unit test is complete, It tests the credit method of the bankaccountimpl.py file

--- Tests/tests.py
+++ Tests/tests.py
@@ -8,18 +8,23 @@ class unit_Tests(object):
         result = subject.get_balance()
         assert result == starting_balance
 
+    def debitReductionMethodTest(self):
+        amountToBeDebited = 30
+        initial_Balance = 100
+        subject = BankAccountImpl(initial_Balance)
+        result = subject.debit(amountToBeDebited)
+        assert result == initial_Balance - amountToBeDebited
 
-    def debitReductionMethod(self):
-        amountToBeDebited=30
-        initial_Balance=100
-        subject=BankAccountImpl(initial_Balance)
-        result=subject.debit(amountToBeDebited)
-        assert result==initial_Balance-amountToBeDebited
-
-
+    def creditAddingMethodTest(self):
+        amountToBeCredited = 50
+        initial_Balance = 200
+        subject = BankAccountImpl(initial_Balance)
+        result = subject.credit(amountToBeCredited)
+        assert result == initial_Balance + amountToBeCredited
 
 
 if __name__ == "__main__":
     obj = unit_Tests()
     obj.testStartingAmount()
-    obj.debitReductionMethod()
+    obj.debitReductionMethodTest()
+    obj.creditAddingMethodTest()
