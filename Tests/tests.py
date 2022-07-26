733903585d80ecb9782d296a14857dc2830fcb70
ProttushPurno <p.purno@uea.ac.uk>
2022-07-26 16:20:26 +0100

    Second Unit test is complete, It tests the debit method of the bankaccountimpl.py file

--- Tests/tests.py
+++ Tests/tests.py
@@ -9,6 +9,17 @@ class unit_Tests(object):
         assert result == starting_balance
 
 
+    def debitReductionMethod(self):
+        amountToBeDebited=30
+        initial_Balance=100
+        subject=BankAccountImpl(initial_Balance)
+        result=subject.debit(amountToBeDebited)
+        assert result==initial_Balance-amountToBeDebited
+
+
+
+
 if __name__ == "__main__":
     obj = unit_Tests()
     obj.testStartingAmount()
+    obj.debitReductionMethod()
